c8af3e00eea4d8cfa330ce7085e68b96a46b1d2a
Yongseok Oh <yongseok.oh@linecorp.com>
2020-11-16 12:38:45 +0000

    qa: add dmclock qos test script that runs qs/tasks/cephfs/* locally
    Signed-off-by: Yongseok Oh <yongseok.oh@linecorp.com>

---
+++ qa/dmclock/start_qos_enabled.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NUM_MDS=2
+VSTART_DEST=~/ceph/build MON=1 OSD=1 MDS=${NUM_MDS} ../src/vstart.sh -l -n -d -x --smallmds --without-dashboard --multimds ${NUM_MDS} 
+
+BUILD_PATH=~/ceph/build
+R=100
+W=100
+L=100
+
+${BUILD_PATH}/bin/ceph fs set a max_mds $NUM_MDS
+
+${BUILD_PATH}/bin/ceph --admin-daemon ${BUILD_PATH}/out/mds.a.asok config set mds_dmclock_mds_qos_enable true
+${BUILD_PATH}/bin/ceph --admin-daemon ${BUILD_PATH}/out/mds.b.asok config set mds_dmclock_mds_qos_enable true
+
+${BUILD_PATH}/bin/ceph --admin-daemon ${BUILD_PATH}/out/mds.a.asok config set mds_dmclock_mds_qos_default_reservation ${R}
+${BUILD_PATH}/bin/ceph --admin-daemon ${BUILD_PATH}/out/mds.a.asok config set mds_dmclock_mds_qos_default_weight ${W}
+${BUILD_PATH}/bin/ceph --admin-daemon ${BUILD_PATH}/out/mds.a.asok config set mds_dmclock_mds_qos_default_limit ${L}
+
+${BUILD_PATH}/bin/ceph --admin-daemon ${BUILD_PATH}/out/mds.b.asok config set mds_dmclock_mds_qos_default_reservation ${R}
+${BUILD_PATH}/bin/ceph --admin-daemon ${BUILD_PATH}/out/mds.b.asok config set mds_dmclock_mds_qos_default_weight ${W}
+${BUILD_PATH}/bin/ceph --admin-daemon ${BUILD_PATH}/out/mds.b.asok config set mds_dmclock_mds_qos_default_limit ${L}
