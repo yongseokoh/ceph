676f46262d3e90a4b02c0fdd81bb1cc57d400ffc
Yongseok Oh <yongseok.oh@linecorp.com>
2020-11-16 12:38:45 +0000

    Add dmclock qos test script that runs qs/tasks/cephfs/* locally

---
+++ qa/dmclock/start_all_test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -x
+
+function run_test() {
+
+    VSTART_LOG_PATH="vstart_log"
+    mkdir -p $VSTART_LOG_PATH
+    VSTART_LOG_PATH="$VSTART_LOG_PATH/$(date +experiment-%Y%m%d-%H:%M)-${3}"
+    mkdir -p $VSTART_LOG_PATH
+
+    #echo $1 $2 $3
+    return
+
+    for testfile in `cat ${1}`
+    do
+        filename=$(basename $testfile)
+        filename="${filename%%.*}"
+
+        echo test name $filename
+        #echo "Wait for debugging, please enter any keys"
+        #read test
+
+        ./start_qos_enabled.sh $2
+
+        python ../qa/tasks/vstart_runner.py tasks.cephfs.${filename}
+
+        mv vstart_runner.log ${VSTART_LOG_PATH}/${filename}.log
+
+        sleep 5
+        ./stop.sh
+
+        sleep 5
+        ./kill-fuse.sh
+
+        sleep 5
+        ./umount_fuse_all.sh
+    done
+}
+
+TEST_FILE=vstart_passed_test.txt
+
+run_test $TEST_FILE true qos-on
+run_test $TEST_FILE false qos-off
