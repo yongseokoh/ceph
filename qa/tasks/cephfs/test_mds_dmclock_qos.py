e6f42ba6837ed761b93581bbbfbd6648e20eca89
jinmyeonglee <jinious1111@naver.com>
2020-11-24 09:58:11 +0900

    qa/mds: revert "Update read_xattrs to enable qos when vid is root(/)"
    This reverts commit 088cb4b7d7dbddea2b76784778f9da3868c8d6ea.

--- qa/tasks/cephfs/test_mds_dmclock_qos.py
+++ qa/tasks/cephfs/test_mds_dmclock_qos.py
@@ -18,9 +18,8 @@ class TestMDSDmclockQoS(CephFSTestCase):
 
     def setUp(self):
         super(TestMDSDmclockQoS, self).setUp()
-        self.fs.set_max_mds(self.MDSS_REQUIRED)
 
-        #  self._fs_cmd("set", self.fs.name, "max_mds", str(self.MDSS_REQUIRED))
+        self._fs_cmd("set", self.fs.name, "max_mds", str(self.MDSS_REQUIRED))
 
         # create test subvolumes
         self.subvolumes = [self.TEST_SUBVOLUME_PREFIX + str(i)\
@@ -61,24 +60,12 @@ class TestMDSDmclockQoS(CephFSTestCase):
     def enable_qos(self):
         self.mds_asok_all(["config", "set", "mds_dmclock_mds_qos_enable", "true"])
 
-    def is_equal_dict(self, a, b, ignore_key=[]):
-        ignore_key = set(ignore_key)
-        for key in a:
-            if key in ignore_key:
-                continue
-            if key not in b:
-                return False
-            if a[key] != b[key]:
-                return False
-        return True
-
     def dump_qos(self):
         """
         Get dump qos from all active mds, and then compare the result for each voluem_id.
         if dump qos results for each volume_id are different, assert it.
         """
         result ={}
-        ignore = ["session_cnt"]
 
         for id in self.fs.mds_ids:
             out = self.fs.mds_asok(["dump", "qos"], mds_id=id)  # out == list of volume qos info
@@ -86,7 +73,7 @@ class TestMDSDmclockQoS(CephFSTestCase):
                 self.assertIn("volume_id", volume_qos)
 
                 if volume_qos["volume_id"] in result:
-                    self.assertTrue(self.is_equal_dict(volume_qos, result[volume_qos["volume_id"]], ignore_key=ignore))
+                    self.assertEqual(volume_qos, result[volume_qos["volume_id"]])
                 result[volume_qos["volume_id"]] = volume_qos
 
         return list(result.values())
@@ -128,44 +115,6 @@ class TestMDSDmclockQoS(CephFSTestCase):
         for info in stat_qos:
             self.assertIn("use_default", info)
             if info["use_default"]:
-                self.assertEqual(info["reservation"], reservation)
-                self.assertEqual(info["limit"], limit)
-                self.assertEqual(info["weight"], weight)
-
-    def test_update_qos_value_root_volume(self):
-        """
-        Update qos 3 values using setfattr.
-        Check its result via getfattr and dump qos.
-        """
-        self.enable_qos()
-
-        reservation, weight, limit = 100, 100, 100
-
-        self.mount_a.umount_wait()
-        self.mount_a.mount(cephfs_mntpt="/")
-
-        self.mount_a.setfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_reservation", str(reservation))
-        self.mount_a.setfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_limit", str(limit))
-        self.mount_a.setfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_weight", str(weight))
-
-        # check updated vxattr using getfattr
-        self.assertEqual(
-                int(float(self.mount_a.getfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_reservation"))),
-                reservation)
-        self.assertEqual(
-                int(float(self.mount_a.getfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_weight"))),
-                weight)
-        self.assertEqual(
-                int(float(self.mount_a.getfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_limit"))),
-                limit)
-
-        stat_qos = self.dump_qos()
-        log.info(stat_qos)
-
-        # check updated dmclock info using dump qos
-        for info in stat_qos:
-            self.assertIn("volume_id", info)
-            if info["volume_id"] == self.get_subvolume_path(self.subvolumes[0]):
                 self.assertEqual(info["reservation"], reservation)
                 self.assertEqual(info["limit"], limit)
                 self.assertEqual(info["weight"], weight)
@@ -232,43 +181,31 @@ class TestMDSDmclockQoS(CephFSTestCase):
                 int(float(self.mount_b.getfattr(self.mount_b.hostfs_mntpt, "ceph.dmclock.mds_limit"))),
                 limit)
 
-    def test_qos_throttling_50(self):
-        import threading
-
-        self.enable_qos()
-
-        reservation, weight, limit = 50, 50, 50
-
-        self.mount_a.setfattr(self.mount_a.hostfs_mntpt, "ceph.dir.pin", str(1))
-
-        self.mount_a.setfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_reservation", str(reservation))
-        self.mount_a.setfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_limit", str(limit))
-        self.mount_a.setfattr(self.mount_a.hostfs_mntpt, "ceph.dmclock.mds_weight", str(weight))
-
-        stat_qos = self.dump_qos()
-        log.info(stat_qos)
-
-        threads = []
-        results = [0]
-
-        threads.append(threading.Thread(target=create_dirs, args=(0, self.mount_a.hostfs_mntpt, self.TEST_DIR_COUNT, results)))
-
-        log.info("IO Testing...")
-
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
-
-        stat_qos = self.dump_qos()
-        log.info(stat_qos)
-
     def test_qos_ratio(self):
         """
         Test QoS throttling.
         For each subvolume, test QoS using thread.
         """
         import threading
+
+        def create_dirs(self, tid, base_path):
+            from os import path, mkdir
+            import time
+
+            base = path.join(base_path, "thread_{0}".format(tid))
+            mkdir(base)
+
+            start = time.time()
+
+            for i in range(self.TEST_DIR_COUNT):
+                mkdir(path.join(base, "dir_{0}".format(i)))
+                for j in range(self.TEST_DIR_COUNT):
+                    mkdir(path.join(base, "dir_{0}".format(i), "subdir_{0}".format(j)))
+
+            elapsed_time = time.time() - start
+
+            log.info("[Thread {0}]: mkdir {1} dirs in {2}secs, OPs/sec: {3}".format(
+                    tid, self.TEST_DIR_COUNT ** 2, elapsed_time, (self.TEST_DIR_COUNT ** 2) / elapsed_time))
 
         self.enable_qos()
 
@@ -289,9 +226,8 @@ class TestMDSDmclockQoS(CephFSTestCase):
         log.info("[Thread 1]: reservation: {0}, weight: {1}, limit {2}".format(reservation * 2, weight * 2, limit * 2))
 
         threads = []
-        results = [0, 0]
-        threads.append(threading.Thread(target=create_dirs, args=(0, self.mount_a.hostfs_mntpt, self.TEST_DIR_COUNT, results)))
-        threads.append(threading.Thread(target=create_dirs, args=(1, self.mount_b.hostfs_mntpt, self.TEST_DIR_COUNT, results)))
+        threads.append(threading.Thread(target=create_dirs, args=(self, 0, self.mount_a.hostfs_mntpt, )))
+        threads.append(threading.Thread(target=create_dirs, args=(self, 1, self.mount_b.hostfs_mntpt, )))
 
         log.info("IO Testing...")
 
@@ -302,28 +238,4 @@ class TestMDSDmclockQoS(CephFSTestCase):
 
         stat_qos = self.dump_qos()
         log.info(stat_qos)
-
-        self.assertGreaterEqual(results[0] * 2, results[1] * 0.8)
-        self.assertLessEqual(results[0] * 2, results[1] * 1.2)
-
-
-def create_dirs(tid, base_path, TEST_DIR_COUNT, results):
-    from os import path, mkdir
-    import time
-
-    base = path.join(base_path, "thread_{0}".format(tid))
-    mkdir(base)
-
-    start = time.time()
-
-    for i in range(TEST_DIR_COUNT):
-        mkdir(path.join(base, "dir_{0}".format(i)))
-        for j in range(TEST_DIR_COUNT):
-            mkdir(path.join(base, "dir_{0}".format(i), "subdir_{0}".format(j)))
-
-    elapsed_time = time.time() - start
-
-    results[tid] = (TEST_DIR_COUNT ** 2) / elapsed_time
-    log.info("[Thread {0}]: mkdir {1} dirs in {2}secs, OPs/sec: {3}".format(
-            tid, TEST_DIR_COUNT ** 2, elapsed_time, (TEST_DIR_COUNT ** 2) / elapsed_time))
 
