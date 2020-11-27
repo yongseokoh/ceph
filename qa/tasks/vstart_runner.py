14e78c405467c0fad77a52c99190d35cf3b32ddd
jinmyeonglee <jinious1111@naver.com>
2020-11-27 01:04:01 +0000

    qa: Add vstart_runner.py option (--enable-mds-qos) to apply qos to cluster, Enable to turn on qos in Filesystem

--- qa/tasks/vstart_runner.py
+++ qa/tasks/vstart_runner.py
@@ -1145,8 +1145,8 @@ class LocalMDSCluster(LocalCephCluster, MDSCluster):
         # FIXME: unimplemented
         pass
 
-    def newfs(self, name='cephfs', create=True):
-        return LocalFilesystem(self._ctx, name=name, create=create)
+    def newfs(self, name='cephfs', create=True, fs_config=None):
+        return LocalFilesystem(self._ctx, name=name, create=create, fs_config=fs_config)
 
     def delete_all_filesystems(self):
         """
@@ -1518,6 +1518,7 @@ def exec_test():
     opt_rotate_logs = False
     global opt_exit_on_test_failure
     opt_exit_on_test_failure = True
+    opt_mds_qos = False
 
     args = sys.argv[1:]
     flags = [a for a in args if a.startswith("-")]
@@ -1559,6 +1560,8 @@ def exec_test():
             opt_rotate_logs = True
         elif f == '--run-all-tests':
             opt_exit_on_test_failure = False
+        elif f == '--enable-mds-qos':
+            opt_mds_qos = True
         else:
             log.error("Unknown option '{0}'".format(f))
             sys.exit(-1)
@@ -1691,6 +1694,11 @@ def exec_test():
     # so that cephfs-data-scan will pick it up too.
     ceph_cluster.set_ceph_conf("global", "mds root ino uid", "%s" % os.getuid())
     ceph_cluster.set_ceph_conf("global", "mds root ino gid", "%s" % os.getgid())
+
+    if opt_mds_qos:
+        ceph_cluster.set_ceph_conf("mds", "mds_dmclock_mds_qos_enable", "true")
+    else:
+        ceph_cluster.set_ceph_conf("mds", "mds_dmclock_mds_qos_enable", "false")
 
     # Monkeypatch get_package_version to avoid having to work out what kind of distro we're on
     def _get_package_version(remote, pkg_name):
