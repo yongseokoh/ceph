a662f163a36dfb2c5eb6efdddfee5e254bafafb6
Jinmyeong Lee <jinmyeong.lee@linecorp.com>
2020-12-09 08:41:10 +0000

    client: Add new message MClientQoS.h & Add dmclock_info to client Inode
    In order for MDSs to know which clients have QoS feature, it needs to be assigned a new feature bit, we temporarily opted 45 (DEFINE_CEPH_FEATURE(45, 1, FS_QOS)).
    Also, add dmclock_info_t to client's Inode.h and update its decoder.
    To sync with MDSs, new message type MClientQoS.h is added.
    
    Signed-off-by: Jinmyeong Lee <jinmyeong.lee@linecorp.com>

--- src/messages/MClientReply.h
+++ src/messages/MClientReply.h
@@ -24,6 +24,8 @@
 #include "include/ceph_features.h"
 #include "common/errno.h"
 
+#include "dmclock/src/dmclock_recs.h"
+
 /***
  *
  * MClientReply - container message for MDS reply to a client's MClientRequest
@@ -133,6 +135,7 @@ struct InodeStat {
   version_t inline_version;
 
   quota_info_t quota;
+  dmclock_info_t dmclock_info;
 
   mds_rank_t dir_pin;
 
@@ -182,6 +185,7 @@ struct InodeStat {
       decode(inline_version, p);
       decode(inline_data, p);
       decode(quota, p);
+      decode(dmclock_info, p);
       decode(layout.pool_ns, p);
       decode(btime, p);
       decode(change_attr, p);
@@ -248,6 +252,13 @@ struct InodeStat {
         decode(quota, p);
       else
         quota = quota_info_t{};
+
+      if (features & CEPH_FEATURE_FS_QOS) {
+        decode(dmclock_info, p);
+      }
+      else {
+        dmclock_info = dmclock_info_t{};
+      }
 
       if ((features & CEPH_FEATURE_FS_FILE_LAYOUT_V2))
         decode(layout.pool_ns, p);
